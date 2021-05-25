c4fbef5a54e7ca4eac67c6f14e154d435981cd11
Neena Mani <neena.mani@gmail.com>
2021-05-25 23:11:10 +1000

    Comments added to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,44 @@
 # PyBank Assignment
-# Importing modules os and csv
+# Import modules os and csv
 import csv
 import os
 
-# Setting the path for the CSV file in csvpath
+# Set the path for the CSV file in csvpath
 csvpath = os.path.join("Resources","budget_data.csv")
 
-# Creating lists to store data and initializing
+# Create lists to store data and initialize
 total_months = 0
 total_amount = 0
 firstline = True
 change_dict = {"Date": [], "Changes": []}
 
+# Function to format amount to $ in the final output
 def currency(amount):
     return ("${}".format(amount))
 
+# Read the csv file and skip / store the header in variable csv_header
 with open(csvpath) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=",")
 	csv_header = next(csvreader)
 
-	for row in csvreader:
+	for row in csvreader:                 # read each row in csv file to calcualte the total months
 		total_months += 1
-		total_amount += int(row[1])
+		total_amount += int(row[1])       # calculate the total profit/loss  
 
-		if firstline: # skip first line
+		if firstline:                     # skip first line while calculating the average change
 			firstline = False
 			old_value = int(row[1])
 		else:
-			current_value = int(row[1])
-			change_dict["Date"].append(row[0])
+			current_value = int(row[1])   # from 2nd row, calculate the differene in profits/loss and save in a dictionary
+			change_dict["Date"].append(row[0])  
 			change_dict["Changes"].append((current_value)-(old_value))
 			old_value = current_value
 
-sum_changes = sum(change_dict["Changes"])
+sum_changes = sum(change_dict["Changes"])   
 num_changes = len(change_dict["Changes"])
-average = round((sum_changes / num_changes),2) 
+average = round((sum_changes / num_changes),2) # find the average of the change in profit/loss using dictionary
 
+# find the greatest increase and decrease in Profits from dictionary
 maximum = max(change_dict["Changes"])
 minimum = min(change_dict["Changes"])
 index_maximum = (change_dict["Changes"].index(maximum))
@@ -43,16 +46,19 @@ index_minimum = (change_dict["Changes"].index(minimum))
 date_maximum = (change_dict["Date"][index_maximum])
 date_minimum = (change_dict["Date"][index_minimum])
 
+# Display the output to show the total months, amount, average change, greatest increase and decrease in profit
 print("Financial Analysis")
 print("-----------------------------")
 print(f"Total months: {total_months}")
-print(f"Total: {currency(total_amount)}")
-print(f"Average Change: {currency(average)}")
-print(f"Greatest Increase in Profits: {date_maximum} ({currency(maximum)})")
-print(f"Greatest Decrease in Profits: {date_minimum} ({currency(minimum)})")
+print(f"Total: {currency(total_amount)}")       # calling function to format currency
+print(f"Average Change: {currency(average)}")   # calling function to format currency
+print(f"Greatest Increase in Profits: {date_maximum} ({currency(maximum)})")  # calling function to format currency
+print(f"Greatest Decrease in Profits: {date_minimum} ({currency(minimum)})")  # calling function to format currency
 
+# Set path for the output text file
 output_file = os.path.join("Analysis","pybank_output.txt")
 
+# Write the output into the output text file
 with open(output_file, 'w') as text:
     text.write("Financial Analysis")
     text.write("\n")
